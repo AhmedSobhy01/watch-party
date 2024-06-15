a35d803a8a19d9975731eddae141e6151574bff6
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-06-16 00:01:23 +0300

    :sparkles: Changed emojis bar size

--- src/components/EmojisBar.vue
+++ src/components/EmojisBar.vue
@@ -25,7 +25,7 @@ onMounted(() => {
             mode: "free",
             slides: {
                 perView: "auto",
-                spacing: 35,
+                spacing: window.innerWidth < 1024 ? 35 : 10,
             },
             dragSpeed: 0.5,
             rubberband: true,
@@ -35,7 +35,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="max-h-24 lg:max-h-20 py-2 keen-slider overflow-hidden pr-4" ref="containerElement">
+    <div class="max-h-24 lg:max-h-16 py-2 keen-slider overflow-hidden pr-4" ref="containerElement">
         <Vue3Lottie class="min-w-16 max-w-20 lg:min-w-[4.5rem] cursor-pointer keen-slider__slide" :animationData="LaughEmojiJSON" :loop="true" :playOnHover="true" @click="() => sendEmoji('laugh')" />
         <Vue3Lottie class="min-w-16 max-w-20 lg:min-w-[4.5rem] cursor-pointer keen-slider__slide" :animationData="AngryEmojiJSON" :loop="true" :playOnHover="true" @click="() => sendEmoji('angry')" />
         <Vue3Lottie class="min-w-16 max-w-20 lg:min-w-[4.5rem] cursor-pointer keen-slider__slide" :animationData="SadEmojiJSON" :loop="true" :playOnHover="true" @click="() => sendEmoji('sad')" />
