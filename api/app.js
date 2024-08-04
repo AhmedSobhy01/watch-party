2fd883b79779312bf6e7de139379090ea778e57a
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-08-05 00:50:15 +0300

    :wrench: Fixed CORS config causing loading issues

--- api/app.js
+++ api/app.js
@@ -21,7 +21,14 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/room", require("./routes/room"));
 
